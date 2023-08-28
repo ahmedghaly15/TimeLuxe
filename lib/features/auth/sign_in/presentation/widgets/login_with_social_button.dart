fc5f13292931e954b7cc16059f9201618a6bc03c
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2023-08-29 02:15:58 +0300

    Add animation to SocialButtons

--- lib/features/auth/sign_in/presentation/widgets/login_with_social_button.dart
+++ lib/features/auth/sign_in/presentation/widgets/login_with_social_button.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:reusable_components/reusable_components.dart';
 
+import '../../../../../core/global/app_colors.dart';
 import '../../../../../core/global/app_text_styles.dart';
 
 class LoginWithSocialButton extends StatelessWidget {
@@ -10,38 +11,68 @@ class LoginWithSocialButton extends StatelessWidget {
     required this.text,
     required this.icon,
     required this.onPressed,
-    this.iconColor = Colors.black,
+    required this.isActive,
   });
 
   final String text;
   final String icon;
-  final Color? iconColor;
+
   final VoidCallback onPressed;
+  final bool isActive;
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        margin: const EdgeInsets.only(bottom: 16),
-        child: MyCustomButton(
-          onPressed: onPressed,
-          height: 48,
-          width: 305,
-          border: Border.all(
-            color: Colors.white,
-            width: 2,
+      child: Stack(
+        children: <Widget>[
+          AnimatedPositioned(
+            height: 48,
+            width: isActive ? 305 : 0,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.fastOutSlowIn,
+            child: Container(
+              decoration: BoxDecoration(
+                color: isActive
+                    ? AppColors.primaryColor
+                    : AppColors.authScaffoldBackgroundColor,
+                borderRadius: const BorderRadius.all(Radius.circular(14)),
+                border: isActive
+                    ? Border.all(
+                        color: AppColors.primaryColor,
+                        width: 2,
+                      )
+                    : Border.all(
+                        color: Colors.white,
+                        width: 2,
+                      ),
+              ),
+            ),
           ),
-          radius: 14,
-          hasPrefix: true,
-          prefixWidget: SvgPicture.asset(
-            icon,
-            color: iconColor,
+          Container(
+            margin: const EdgeInsets.only(bottom: 16),
+            child: MyCustomButton(
+              onPressed: onPressed,
+              height: 48,
+              width: 305,
+              border: Border.all(
+                color: Colors.white,
+                width: 2,
+              ),
+              radius: 14,
+              hasPrefix: true,
+              prefixWidget: SvgPicture.asset(
+                icon,
+                color: isActive ? Colors.white : Colors.black,
+              ),
+              child: Text(
+                text,
+                style: isActive
+                    ? AppTextStyles.textStyle16.copyWith(color: Colors.white)
+                    : AppTextStyles.textStyle16,
+              ),
+            ),
           ),
-          child: Text(
-            text,
-            style: AppTextStyles.textStyle16,
-          ),
-        ),
+        ],
       ),
     );
   }
--- lib/features/auth/sign_in/presentation/widgets/social_buttons.dart
+++ lib/features/auth/sign_in/presentation/widgets/social_buttons.dart
@@ -4,7 +4,7 @@ import '../../../../../core/global/app_assets.dart';
 import '../cubit/sign_in_cubit.dart';
 import 'login_with_social_button.dart';
 
-class SocialButtons extends StatelessWidget {
+class SocialButtons extends StatefulWidget {
   const SocialButtons({
     super.key,
     required this.cubit,
@@ -13,21 +13,75 @@ class SocialButtons extends StatelessWidget {
   final SignInViewCubit cubit;
 
   @override
+  State<SocialButtons> createState() => _SocialButtonsState();
+}
+
+class _SocialButtonsState extends State<SocialButtons> {
+  bool isFacebookButtonClicked = false;
+  bool isGoogleButtonClicked = false;
+  bool isAppleButtonClicked = false;
+
+  @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         LoginWithSocialButton(
-          onPressed: () {},
+          onPressed: () {
+            setState(() {
+              isFacebookButtonClicked = true;
+            });
+
+            Future.delayed(
+              const Duration(seconds: 1),
+              () {
+                debugPrint("Facebook Button is Clicked");
+                setState(() {
+                  isFacebookButtonClicked = false;
+                });
+              },
+            );
+          },
+          isActive: isFacebookButtonClicked,
           text: "Sign in with Facebook",
           icon: AppAssets.iconsUilFacebook,
         ),
         LoginWithSocialButton(
-          onPressed: () => cubit.signInWithGoogle(),
+          onPressed: () {
+            setState(() {
+              isGoogleButtonClicked = true;
+            });
+
+            Future.delayed(
+              const Duration(seconds: 1),
+              () {
+                widget.cubit.signInWithGoogle();
+                setState(() {
+                  isGoogleButtonClicked = false;
+                });
+              },
+            );
+          },
+          isActive: isGoogleButtonClicked,
           text: "Sign in with Google",
           icon: AppAssets.iconsGgGoogle,
         ),
         LoginWithSocialButton(
-          onPressed: () {},
+          onPressed: () {
+            setState(() {
+              isAppleButtonClicked = true;
+            });
+
+            Future.delayed(
+              const Duration(seconds: 1),
+              () {
+                debugPrint("Apple Button is Clicked");
+                setState(() {
+                  isAppleButtonClicked = false;
+                });
+              },
+            );
+          },
+          isActive: isAppleButtonClicked,
           text: "Sign in with Apple",
           icon: AppAssets.iconsBiApple,
         ),
